3ba53502ce272d1d0ce043b1a215a7c194d13cf7
Rodolpho Alves <rrpa15@live.com>
2020-12-07 00:52:32 -0300

    Testes clientes

--- src/test/java/com/ufpb/projetoAluguelVeiculo/TesteLojas.java
+++ src/test/java/com/ufpb/projetoAluguelVeiculo/TesteLojas.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 
+import com.google.gson.Gson;
 import com.ufpb.projetoAluguelVeiculo.entities.Aluguel;
 import com.ufpb.projetoAluguelVeiculo.entities.Cliente;
 import com.ufpb.projetoAluguelVeiculo.entities.Loja;
@@ -152,8 +153,97 @@ public class TesteLojas {
     private String TEXTO_ESPERADO_LOJA_NUMERO_INVALIDO = "loja Loja1 sem numero.";
 
     
+    
+    // ClientesService
 
+    private String TEXTO_ESPERADO_CLIENTE_NOME_INVALIDO = "Cliente sem nome.";
+    private String TEXTO_ESPERADO_CLIENTE_CPF_INVALIDO = "Cliente Cliente1 sem cpf.";
+    private String TEXTO_ESPERADO_CLIENTE_EMAIL_INVALIDO = "Cliente Cliente1 sem email.";
+    private String TEXTO_ESPERADO_CLIENTE_TELEFONE_INVALIDO = "Cliente Cliente1 sem telefone.";
 
+    @Test
+    public void validarNomeClienteInvalido() {
+        Cliente clienteNomeVazio = clienteCompleto;
+        clienteNomeVazio.setNome("");
+        validarCliente(TEXTO_ESPERADO_CLIENTE_NOME_INVALIDO, clienteNomeVazio);
+
+        Cliente clienteNomeNulo = clienteCompleto;
+        clienteNomeNulo.setNome(null);
+        validarCliente(TEXTO_ESPERADO_CLIENTE_NOME_INVALIDO, clienteNomeNulo);
+    }
+
+    @Test
+    public void validarCpfClienteInvalido() {
+        Cliente clienteCpfVazio = clienteCompleto;
+        clienteCpfVazio.setCpf("");
+        validarCliente(TEXTO_ESPERADO_CLIENTE_CPF_INVALIDO, clienteCpfVazio);
+
+        Cliente clienteCpfNulo = clienteCompleto;
+        clienteCpfNulo.setCpf(null);
+        validarCliente(TEXTO_ESPERADO_CLIENTE_CPF_INVALIDO, clienteCpfNulo);
+    }
+
+    @Test
+    public void validarEmailClienteInvalido() {
+        Cliente clienteEmailVazio = clienteCompleto;
+        clienteEmailVazio.setEmail("");
+        validarCliente(TEXTO_ESPERADO_CLIENTE_EMAIL_INVALIDO, clienteEmailVazio);
+
+        Cliente clienteEmailNulo = clienteCompleto;
+        clienteEmailNulo.setEmail(null);
+        validarCliente(TEXTO_ESPERADO_CLIENTE_EMAIL_INVALIDO, clienteEmailNulo);
+    }
+
+    @Test
+    public void validarTelefoneClienteInvalido() {
+        Cliente clienteTelefoneVazio = clienteCompleto;
+        clienteTelefoneVazio.setTelefone("");
+        validarCliente(TEXTO_ESPERADO_CLIENTE_TELEFONE_INVALIDO, clienteTelefoneVazio);
+
+        Cliente clienteTelefoneNulo = clienteCompleto;
+        clienteTelefoneNulo.setTelefone(null);
+        validarCliente(TEXTO_ESPERADO_CLIENTE_TELEFONE_INVALIDO, clienteTelefoneNulo);
+    }
+
+    // addClienteLoja
+    @Test
+    public void testarAdicionarCliente() {
+        ls.deletarLojas();
+        ls.adicionarLoja(lojaCompleta1);
+        String cpfTeste = CPF_CLIENTE;
+        cs.adicionarCliente(clienteCompleto, CNPJ_LOJA1);
+        assertEquals(cs.buscarClientePorCpf(
+            CPF_CLIENTE, 
+            CNPJ_LOJA1
+        ).getCpf(), cpfTeste);
+    }
+
+    // listarClientesPorCnpj
+    @Test
+    public void testarListagemDeClientesPorCnpj() {
+        ls.deletarLojas();
+        ls.adicionarLoja(lojaCompleta1);
+
+        Cliente c1 = cs.adicionarCliente(clienteCompleto, CNPJ_LOJA1);
+        Cliente c2 = cs.adicionarCliente(clienteCompleto2, CNPJ_LOJA1);
+        ArrayList<Cliente> clientesAux = arrToArrayList(c1, c2);
+        
+        Gson gson = new Gson();
+        String listaAux = gson.toJson(clientesAux);
+        String listaRecebida = gson.toJson(cs.listarClientes(lojaCompleta1.getCnpj()));
+        assertEquals(listaAux, listaRecebida);
+    }
+
+    // deletarCliente
+    @Test
+    public void testarDeletarCliente() {
+        ls.deletarLojas();
+        ls.adicionarLoja(lojaCompleta1);
+        String cpfTeste = CPF_CLIENTE;
+        cs.adicionarCliente(clienteCompleto, CNPJ_LOJA1);
+        Boolean deletado = cs.removerCliente(cpfTeste, CNPJ_LOJA1);
+        assertEquals(true, deletado);
+    }
 
 
     // AlugueisService
