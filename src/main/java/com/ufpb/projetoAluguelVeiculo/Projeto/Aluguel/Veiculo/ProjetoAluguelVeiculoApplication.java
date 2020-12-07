a72ad6804c5eef3039817456ac673ce663a56b64
Gabriel-J-code <gabrieldejesus00@gmail.com>
2020-12-06 22:42:43 -0300

    Initial commit

---
+++ src/main/java/com/ufpb/projetoAluguelVeiculo/Projeto/Aluguel/Veiculo/ProjetoAluguelVeiculoApplication.java
@@ -0,0 +1,13 @@
+package com.ufpb.projetoAluguelVeiculo.Projeto.Aluguel.Veiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAluguelVeiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAluguelVeiculoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufpb/projetoAluguelVeiculo/Projeto/Aluguel/Veiculo/ProjetoAluguelVeiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpb.projetoAluguelVeiculo.Projeto.Aluguel.Veiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAluguelVeiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
